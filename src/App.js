1171e31ca6032971d18b0b9fe8a2fcb4afc58408
Aditya-Kanojia <aditya05kanojia@gmial.com>
2023-12-04 20:47:13 +0530

    inital commit all inital files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h3>Shopping</h3>
+     <p>let's Buy Somthing For You</p>
+    </div>
+  );
+}
+
+export default App;
